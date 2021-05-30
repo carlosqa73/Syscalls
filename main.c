1051d6597917ad12b597a4694c8a88cca6f070b9
Carlos <carlosqa73@hotmail.com>
2021-05-30 12:32:03 -0500

    Manejo de errores

--- main.c
+++ main.c
@@ -18,23 +18,27 @@ int main(int argc, char* argv[]){
 	int bytes;
 
 	if(argc == 1){			//Se asume que este caso es cuando se utiliza pipe
-		bytes = read(0, buff, 1024);
-		write(1, buff, bytes);
+		bytes = read(0, buff, 1024);		//Se lee desde stdin
+		if(bytes == -1){
+			perror("Error al leer desde stdin\n");
+			exit(EXIT_FAILURE);
+		}
+		write(1, buff, bytes);			//Se escribe en stdout
 		return 0;
 	}
 
 	else{
 
-		int fd = open(argv[1], O_RDONLY);
+		int fd = open(argv[1], O_RDONLY);		//Se abre el archivo
 
-		if(fd == -1){
+		if(fd == -1){					//Si el descriptor es -1 entonces hubo un error en open
 			perror("Error al abrir el archivo\n");
 			exit(EXIT_FAILURE);
 		}
 		//bytes = read(fd, argv[1], 1000);
 		//close(fd);
 		while((bytes = read(fd, buff, 1024)) > 0){
-			write(1, buff, bytes);			//El 1 es stdout
+			write(1, buff, bytes);			//Se escribe en stdout
 		}
 		close(fd);
 	}
