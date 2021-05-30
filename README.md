d1ea0948f12b05263c54ba078b279f593a9c56c3
Carlos <carlosqa73@hotmail.com>
2021-05-30 12:25:58 -0500

    Programa terminado

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Syscalls
+# Syscalls  
+Este es un programa que utiliza funciones wrappers como open(), read() y write()  
+para mostrar contenidos de archivos por stdout, ademas de leer contenido de stdin  
+y mostrarlos tambien en stdout.  
+
+## Compilacion  
+Para compilar el programa:  
+```bash
+make
+```
+Para hacer clean:  
+```bash
+make clean
+```
+
+## Uso
+Para ejecutar el programa:  
+```bash
+./mycat filename
+```
+
+Para leer desde stdin:
+```bash
+sort filename | ./mycat
+```
--- main.c
+++ main.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
 
 int main(int argc, char* argv[]){
-	return 0;
+
+	if(argc > 3){
+		fprintf(stderr, "Error de uso: ./mycat filename\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	char buff[1024];
+	int bytes;
+
+	if(argc == 1){			//Se asume que este caso es cuando se utiliza pipe
+		bytes = read(0, buff, 1024);
+		write(1, buff, bytes);
+		return 0;
+	}
+
+	else{
+
+		int fd = open(argv[1], O_RDONLY);
+
+		if(fd == -1){
+			perror("Error al abrir el archivo\n");
+			exit(EXIT_FAILURE);
+		}
+		//bytes = read(fd, argv[1], 1000);
+		//close(fd);
+		while((bytes = read(fd, buff, 1024)) > 0){
+			write(1, buff, bytes);			//El 1 es stdout
+		}
+		close(fd);
+	}
+
+	//printf("Se leyeron %d bytes\n", bytes);
 }
